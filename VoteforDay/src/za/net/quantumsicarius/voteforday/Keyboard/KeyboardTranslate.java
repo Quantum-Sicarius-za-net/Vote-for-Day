9c8b6bf6cb8b025803638d48596900b909d32aaa
Quantum-Sicarius <thomas@visco.co.za>
2012-04-14 18:56:52 +0200

    Added the new classes

---
+++ VoteforDay/src/za/net/quantumsicarius/voteforday/Keyboard/KeyboardTranslate.java
@@ -0,0 +1,122 @@
+package za.net.quantumsicarius.voteforday.Keyboard;
+
+import org.getspout.spoutapi.keyboard.Keyboard;
+
+public class KeyboardTranslate {
+	public Keyboard Translate(String string) {
+		if (string.trim().equalsIgnoreCase("a")) {
+			return Keyboard.KEY_A;
+		}
+		else if(string.trim().equalsIgnoreCase("b")) {
+			return Keyboard.KEY_B;
+		}
+		else if(string.trim().equalsIgnoreCase("c")) {
+			return Keyboard.KEY_C;
+		}
+		else if(string.trim().equalsIgnoreCase("d")) {
+			return Keyboard.KEY_D;
+		}
+		else if(string.trim().equalsIgnoreCase("e")) {
+			return Keyboard.KEY_E;
+		}
+		else if(string.trim().equalsIgnoreCase("f")) {
+			return Keyboard.KEY_F;
+		}
+		else if(string.trim().equalsIgnoreCase("g")) {
+			return Keyboard.KEY_G;
+		}
+		else if(string.trim().equalsIgnoreCase("h")) {
+			return Keyboard.KEY_H;
+		}
+		else if(string.trim().equalsIgnoreCase("i")) {
+			return Keyboard.KEY_I;
+		}
+		else if(string.trim().equalsIgnoreCase("j")) {
+			return Keyboard.KEY_J;
+		}
+		else if(string.trim().equalsIgnoreCase("k")) {
+			return Keyboard.KEY_K;
+		}
+		else if(string.trim().equalsIgnoreCase("l")) {
+			return Keyboard.KEY_L;
+		}
+		else if(string.trim().equalsIgnoreCase("m")) {
+			return Keyboard.KEY_M;
+		}
+		else if(string.trim().equalsIgnoreCase("n")) {
+			return Keyboard.KEY_N;
+		}
+		else if(string.trim().equalsIgnoreCase("o")) {
+			return Keyboard.KEY_O;
+		}
+		else if(string.trim().equalsIgnoreCase("p")) {
+			return Keyboard.KEY_P;
+		}
+		else if(string.trim().equalsIgnoreCase("q")) {
+			return Keyboard.KEY_Q;
+		}
+		else if(string.trim().equalsIgnoreCase("r")) {
+			return Keyboard.KEY_R;
+		}
+		else if(string.trim().equalsIgnoreCase("s")) {
+			return Keyboard.KEY_S;
+		}
+		else if(string.trim().equalsIgnoreCase("t")) {
+			return Keyboard.KEY_T;
+		}
+		else if(string.trim().equalsIgnoreCase("u")) {
+			return Keyboard.KEY_U;
+		}
+		else if(string.trim().equalsIgnoreCase("x")) {
+			return Keyboard.KEY_X;
+		}
+		else if(string.trim().equalsIgnoreCase("w")) {
+			return Keyboard.KEY_W;
+		}
+		else if(string.trim().equalsIgnoreCase("y")) {
+			return Keyboard.KEY_Y;
+		}
+		else if(string.trim().equalsIgnoreCase("z")) {
+			return Keyboard.KEY_Z;
+		}
+		else if(string.trim().equalsIgnoreCase("f1")) {
+			return Keyboard.KEY_F1;
+		}
+		else if(string.trim().equalsIgnoreCase("f2")) {
+			return Keyboard.KEY_F2;
+		}
+		else if(string.trim().equalsIgnoreCase("f3")) {
+			return Keyboard.KEY_F3;
+		}
+		else if(string.trim().equalsIgnoreCase("f4")) {
+			return Keyboard.KEY_F4;
+		}
+		else if(string.trim().equalsIgnoreCase("f5")) {
+			return Keyboard.KEY_F5;
+		}
+		else if(string.trim().equalsIgnoreCase("f6")) {
+			return Keyboard.KEY_F6;
+		}
+		else if(string.trim().equalsIgnoreCase("f7")) {
+			return Keyboard.KEY_F7;
+		}
+		else if(string.trim().equalsIgnoreCase("f8")) {
+			return Keyboard.KEY_F8;
+		}
+		else if(string.trim().equalsIgnoreCase("f9")) {
+			return Keyboard.KEY_F9;
+		}
+		else if(string.trim().equalsIgnoreCase("f10")) {
+			return Keyboard.KEY_F10;
+		}
+		else if(string.trim().equalsIgnoreCase("f11")) {
+			return Keyboard.KEY_F11;
+		}
+		else if(string.trim().equalsIgnoreCase("f12")) {
+			return Keyboard.KEY_F12;
+		}
+		else {
+			return Keyboard.KEY_V;
+		}
+	}
+}
---
+++ VoteforDay/src/za/net/quantumsicarius/voteforday/VoteStartHandler/VoteStartHandler.java
@@ -0,0 +1,123 @@
+package za.net.quantumsicarius.voteforday.VoteStartHandler;
+
+import java.util.HashMap;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import za.net.quantumsicarius.voteforday.Chat.Chat;
+import za.net.quantumsicarius.voteforday.Config.Config;
+import za.net.quantumsicarius.voteforday.Logger.LogMain;
+
+public class VoteStartHandler {
+	
+	Plugin vfd;
+	
+	// Define chat object
+	Chat chat;
+	
+	// Define logger object
+	LogMain log;
+	
+	// Define configuration object
+	Config config;
+	
+	// Define world object
+	World current_world;
+	
+	// Create HashMaps
+	HashMap <Player, Boolean> voted = new HashMap<Player, Boolean>();
+	HashMap <Player, Boolean> votes = new HashMap<Player, Boolean>();	
+	HashMap <World, Boolean> vote_session_world = new HashMap<World, Boolean>();
+
+	public VoteStartHandler(Plugin plugin, Config config_class, LogMain log_class, Chat chat_class) {
+		// Instantiate plugin object
+		vfd = plugin;
+		// Instantiate configuration object
+		config = config_class;
+		// Instantiate chat object
+		chat = chat_class;
+		// Instantiate log object
+		log = log_class;
+	}
+	
+	// Vote_session_world = World vote session active
+	// Votes = Player's votes (yes,no)
+	// Voted = Player has voted
+	public boolean onCommand(Player player, HashMap<World, Boolean> hashmap_vote_session_world, HashMap<Player, Boolean> hashmap_votes, HashMap<Player, Boolean> hashmap_voted) {
+		
+		voted = hashmap_voted;
+		votes = hashmap_votes;
+		vote_session_world = hashmap_vote_session_world;
+		
+		if (player.hasPermission("") | player.isOp()) {
+			if(testRunVote(player)) {
+				// Check keys
+				if (vote_session_world.containsKey(player.getWorld())) {
+					// If current world is active ignore player's vote start
+					if (vote_session_world.get(player.getWorld())) {
+						player.sendMessage(chat.chatWarning("There is already an active vote!"));
+					}
+					else {
+						log.logDebug("Player: " + player.getName() + " has started a vote!");
+						player.sendMessage(chat.chatInfo("Started Vote!"));	
+						
+						vote_session_world.put(player.getWorld(), true);
+						log.logDebug("Started a vote session in: " + player.getWorld().getName());
+						
+						return true;					
+					}
+				}
+				else {
+					log.logDebug("Player: " + player.getName() + " has started a vote!");
+					player.sendMessage(chat.chatInfo("You Started the Vote!"));	
+					
+					vote_session_world.put(player.getWorld(), true);
+					log.logDebug("Started a vote session in: " + player.getWorld().getName());
+					
+					return true;					
+				}
+			}
+		}
+		else {
+			player.sendMessage(chat.chatSevere("You are not permitted to start a vote!"));
+		}
+		return false;
+	}
+	
+	// Test to see if its night
+	private boolean testRunVote(Player player) {
+			
+		current_world = player.getWorld();
+			
+		// Check if the world environment is allowed
+		if(current_world.getEnvironment() == World.Environment.NORMAL) {
+			if  (current_world.getTime() > config.getVoteAllowStartTime()) {
+				return true;
+			}
+			else {
+				log.logDebug("Player: " + player.getName() + " has tried to vote during day time!");
+				player.sendMessage(chat.chatWarning("Can't vote when its day!"));
+				return false;
+			}
+		}
+		else {
+			log.logDebug("Player: " + player.getName() + " has tried to start a vote in an illegal world environment! World Environment: " + current_world.getEnvironment());
+			player.sendMessage(chat.chatSevere("You can only use this command in a Normal World!"));
+			return false;
+		}
+	}
+	
+	public HashMap<Player, Boolean> voted() {
+		return voted;
+	}
+	
+	public HashMap<Player, Boolean> votes() {
+		return votes;
+	}
+	
+	public HashMap<World, Boolean> vote_session_world() {
+		return vote_session_world;
+	}
+}
